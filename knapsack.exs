b0b4a527b03f0eb7db49f67529d31f8a6265a0d8
Utsav Gupta <mail@utsavgupta.in>
2023-08-30 22:09:18 +0530

    refactor the knapsack solution

--- knapsack.exs
+++ knapsack.exs
@@ -1,16 +1,18 @@
+# Solution to the 0-1 Knapsack problem using recursive functions
 defmodule Knapsack do
 
-  def solve(c, wt, val, n), do: calc(c, wt, val, n)
+  # c = capacity, wt = weights, v = values
+  def solve(c, wt, val), do: calc(c, wt, val)
 
-  defp calc(0, _, _, _), do: 0
+  defp calc(0, _, _), do: 0
 
-  defp calc(_, _, _, 0), do: 0
+  defp calc(_, [], []), do: 0
 
-  defp calc(c, [w|wt], [v|val], n) when c-w >= 0 do
-    [v + calc(c-w, wt, val, n-1), calc(c, wt, val, n-1)] |> Enum.max
+  defp calc(c, [w|wt], [v|val]) when c-w >= 0 do
+    [v + calc(c-w, wt, val), calc(c, wt, val)] |> Enum.max
   end
 
-  defp calc(c, [_|wt], [_|val], n), do: calc(c, wt, val, n-1)
+  defp calc(c, [_|wt], [_|val]), do: calc(c, wt, val)
 end
 
-Knapsack.solve(4, [5,4,1], [1, 2, 3], 3) |> IO.puts
+Knapsack.solve(4, [5,4,1], [1, 2, 3]) |> IO.puts
